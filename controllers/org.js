3d6568dc32c54ea4322de6925649b0f362f91b9a
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2022-12-10 10:56:14 +0700

    create organization api

--- controllers/org.js
+++ controllers/org.js
@@ -4,25 +4,29 @@ const now = require("./../utils/time_now")
 // List my org by id
 async function listMyOrg(req, res) {
     try {
-        const orgID = req.params.id;
-        const data = await listOrgModel.findById(orgID);
-        res.status(200, )
+        const orgID = req.params.userID;
+        const data = await listOrgModel.find({ userID: orgID });
+        if (data.length == 0) {
+            return res.status(400).send({ message: "You're not in any organize" })
+
+        }
+        return res.status(200).send(data)
     }
     catch (error) {
-        res.status(500);
+        return res.status(500).send();
     }
 }
 
 // Create new org 
 async function createNewOrg(req, res) {
     try {
-        const { orgName, orgDescription, orgCreatorEmail, orgCreatorID } = req.body;
-        if (!(orgName && orgDescription && orgCreatorEmail && orgCreatorID)) {
-            res.status(400).json({ message: "Please input all fields" });
+        const { orgName, orgDescription, orgCreatorEmail, orgCreatorID, orgCreatorFirstName, orgCreatorLastName } = req.body;
+        if (!(orgName && orgDescription && orgCreatorEmail && orgCreatorID&& orgCreatorFirstName&& orgCreatorLastName)) {
+            return res.status(400).json({ message: "Please input all fields" });
         }
         const oldOrg = await listOrgModel.findOne({ orgName });
         if (oldOrg) {
-            res.status(400).json({ message: "Already organization in database" });
+            return res.status(400).json({ message: "Already organization in database" });
         }
         const date = now()
         // Create org in our database
@@ -31,13 +35,23 @@ async function createNewOrg(req, res) {
             orgCreatedTime: date,
             orgDescription,
             orgCreatorID,
-            orgCreatorEmail
+            orgCreatorEmail,
+            orgCreatorFirstName,
+            orgCreatorLastName,
+            orgMembers : [
+                {
+                    orgMemberID : orgCreatorID,
+                    orgMemberEmail : orgCreatorEmail,
+                    orgMemberFirstName : orgCreatorFirstName,
+                    orgMemberLastName : orgCreatorLastName
+                }
+            ]
         });
 
-        res.status(200).send({ message: "insert success", orgName: orgName, orgCreatedTime: date, orgDescription: orgDescription, orgCreatorID: orgCreatorID, orgCreatorEmail: orgCreatorEmail })
+        return res.status(200).send({ message: "insert success", orgName: orgName, orgCreatedTime: date, orgDescription: orgDescription, orgCreatorID: orgCreatorID, orgCreatorEmail: orgCreatorEmail })
     }
     catch (error) {
-        res.status(500).send();
+        return res.status(500).send();
     }
 }
 
--- models/org.js
+++ models/org.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const orgSchema = new mongoose.Schema({
     orgName: { type: String },
     orgCreatedTime: { type: String },
-    orgDescription: { type: String, default : null },
+    orgDescription: { type: String, default: null },
     orgCreatorID: { type: String },
     orgCreatorEmail: { type: String },
+    orgCreatorFirstName: { type: String },
+    orgCreatorLastName: { type: String },
+    orgMembers: { type: Array },
 });
 
 const listOrgModel = mongoose.model('listOrgazination', orgSchema)
 
-module.exports = {listOrgModel}
+module.exports = { listOrgModel }
